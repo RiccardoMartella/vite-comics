9f11bf4cfda1053ad81e8e18f1300f5e86775268
RiccardoMartella <riccardomartella77@gmail.com>
2023-05-03 14:23:33 +0200

    creazine navbar

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import NavBar from './components/NavBar.vue';
+
+
+export default{
+    name: "AppVue",
+    data() {
+        return {};
+    },
+    components: { NavBar }
+}
+</script>
+
+
+<template>
+  <div>
+    <NavBar />
+
+  </div>
+</template>
+
+<style lang="scss">
+  @use './style/main.scss';
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<script>
+
+export default{
+    name: "NavBar",
+    data() {
+        return {
+            
+        }
+    }
+}
+
+</script>
+
+<template>
+    <nav>
+        <img src="./../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOIP</li>
+        </ul>
+    </nav>
+
+</template>
+
+<style scoped>
+    
+</style>
