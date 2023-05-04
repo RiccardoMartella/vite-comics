f2bce0635bb2b3db5ade074ea754133379ac9f4b
RiccardoMartella <riccardomartella77@gmail.com>
2023-05-04 15:38:26 +0200

    1 prova (disposizione card senza props)

--- src/components/CardsComp.vue
+++ src/components/CardsComp.vue
@@ -30,49 +30,49 @@ export default{
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                 "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
                  "price": "$3.99",
                  "series": "Batman",
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg/revision/latest?cb=20080809201655",
+                 "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
                  "price": "$2.99",
                  "series": "Batman Beyond",
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg/revision/latest/scale-to-width-down/1200?cb=20130627211607",
+                 "thumb":"https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
                  "price": "$3.99",
                  "series": "Batman/Superman",
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg/revision/latest?cb=20200929145132",
+                 "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
                  "price": "$4.99",
                  "series": "Batman/Superman Annual",
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg/revision/latest?cb=20200929124651",
+                 "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
                  "price": "$5.99",
                  "series": "Batman: The Joker War Zone",
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg/revision/latest?cb=20201123054610",
+                 "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
                  "price": "$6.99",
                  "series": "Batman: Three Jokers",
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg/revision/latest?cb=20201124094800",
+                 "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
                  "price": "$4.99",
                  "series": "Batman: White Knight Presents: Harley Quinn",
                  "type": "comic book"
                },
                {
-                 "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg/revision/latest?cb=20160303223223",
+                 "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
                  "price": "$16.99",
                  "series": "Catwoman",
                  "type": "graphic novel"
@@ -85,10 +85,14 @@ export default{
 </script>
 
 <template>
-   <div>
-      <div class="contenitore-cards" v-for=" element in Cards">
-    {{ element.price }}</div>
-   </div>
+    <div>
+        <div class="contenitore-margin">
+           <div class="contenitore-cards" v-for="(element,index) in Cards" :key="index">
+             <img v-bind:src="element.thumb" alt="">
+             {{ element.price }}
+            </div>
+        </div>
+    </div>
 </template>
   
 
@@ -96,12 +100,27 @@ export default{
 
 <style scoped lang="scss">
    div{
-    display: flex;
-    height: 500px;
-    background-color: rgb(18, 18, 18);
+     display: flex;
+     flex-wrap: wrap;
+     gap:10px;
+     background-color: rgb(18, 18, 18);
    }
 
+   .contenitore-margin{
+      
+      width: 67%;
+      margin: auto;
+      padding: 40px 0;
+    }
+   
    .contenitore-cards{
-    
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: calc(100% / 6 - 10px);
+   }
+
+   img{
+    height: 180px;
    }
 </style>
