20f955ea90cf6d42d60d422ade75e61c5c2d6440
RiccardoMartella <riccardomartella77@gmail.com>
2023-05-04 15:13:30 +0200

    creazione section (12 cards)

--- src/components/SubNavBar.vue
+++ src/components/SubNavBar.vue
@@ -1,20 +1,28 @@
 <script>
 
+import CardsComp from '../components/CardsComp.vue';
+
 export default{
     name: "SubNavBar",
     data() {
-        return {
-            
-        }
-    }
+        return {};
+      },
+      components: { 
+        CardsComp,
+     }
 }
 
 </script>
 
 <template>
+  <div class="contenitore-sfondo">
     <div>
-      <h1>Content goes here</h1>
+      <h1>COURRENT SERIES</h1>
     </div>
+  </div>
+  <div>
+    <CardsComp/>
+  </div>
 </template>
   
 
@@ -24,14 +32,23 @@ export default{
 </style>
 
 <style scoped lang="scss">
-  div{
-    display: flex;
-    background-color: rgb(28, 28, 28);
-    height: 15%;
+  .contenitore-sfondo{
+    background-image:url(../assets/img/jumbotron.jpg);
+    height: 42%;
+    position: relative;
+     div{
+      background-color: rgb(95, 99, 255);
+      width: 300px;
+      position: absolute;
+      left:320px;
+      top: 355px;
+     }
     h1{
       color: white;
       width: 67%;
       margin: auto;
+      font-size: 20px;
+      padding: 5px;
     }
   }
 </style>
